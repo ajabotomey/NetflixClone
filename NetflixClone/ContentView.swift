897be731a515040d8025d1196f66484333073ef9
ajabotomey <killerbyte_93@hotmail.com>
2024-01-09 18:07:12 +1000

    Initial Commit

---
+++ NetflixClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetflixClone
+//
+//  Created by Andrew Abotomey on 9/1/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Andrew Abotomey on 9/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
